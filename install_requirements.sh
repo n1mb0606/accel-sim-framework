da43ac7102984ca32eb9a323ad281e80ead49566
n1mb0606 <n1mb0606@gmail.com>
2024-07-31 13:18:17 +0900

    Update shell

---
+++ install_requirements.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+
+sudo apt-get update &&
+sudo apt-get install  -y wget build-essential xutils-dev bison zlib1g-dev flex \
+      libglu1-mesa-dev git g++ libssl-dev libxml2-dev libboost-all-dev git g++ \
+      libxml2-dev vim python-setuptools build-essential python-pip &&
+python3 -m venv .venv && 
+source .venv/bin/activate &&
+pip3 install -r requirements.txt &&
+git clone git@github.com:n1mb0606/gpu-app-collection.git
---
+++ set_environment.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+export CUDA_INSTALL_PATH=/usr/local/cuda-11.8
+export PATH=$CUDA_INSTALL_PATH/bin:$PATH
+
+soruce .venv/bin/activate
