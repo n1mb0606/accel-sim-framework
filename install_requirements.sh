3eade28a09573d4f55d5fcc52fd7d2586b09b33d
n1mb0606 <n1mb0606@gmail.com>
2024-08-02 13:37:12 +0900

    Add torch dep

--- install_requirements.sh
+++ install_requirements.sh
@@ -7,4 +7,5 @@ sudo apt-get install  -y wget build-essential xutils-dev bison zlib1g-dev flex \
 python3 -m venv .venv && 
 source .venv/bin/activate &&
 pip3 install -r requirements.txt &&
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 &&
 git clone git@github.com:n1mb0606/gpu-app-collection.git
